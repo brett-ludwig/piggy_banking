8f1d769f422763d00395b2d988e2a03fd8999648
Brett <brett.a.ludwig@gmail.com>
2020-01-26 08:14:04 -0500

    almost showcase time

---
+++ src/PiggyBank.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Person,
+} from 'blockstack';
+
+export default class PiggyBank extends Component{
+  render(){
+    return (
+      <div class='piggy-bank'>
+        <img src='https://image.shutterstock.com/image-vector/vector-cartoon-piggy-bank-260nw-169589390.jpg' width='128' height='128'></img>
+      </div>
+    );
+  }
+}
